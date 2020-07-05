136bfb859a3c0ef0df0d13bd1422486ae70b7ebf
rhythm jayee <jayeerythm8@gmail.com>
2020-07-06 01:23:37 +0530

    user register+jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const { check,validationResult } = require('express-validator');
 const router=express.Router();
 const gravatar=require("gravatar");
 const bcrypt=require("bcryptjs");
+const jwt=require("jsonwebtoken");
+const config=require("config");
 
 const User=require("../../models/User");
 
@@ -43,8 +45,19 @@ router.post('/',[
 
         await user.save();
 
-        res.send("User Registered")
-
+        // res.send("User Registered")
+        const payload={
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(payload,
+            config.get('jwtsecret'),
+            {expiresIn:3600},
+            (err,token)=>{
+                if(err) throw err;
+                res.json({token});
+            });
 
     }catch(err){
         console.log(err.message);
