6e969181e24a193b899d47d956536564fdc2b5eb
rhythm jayee <jayeerythm8@gmail.com>
2020-07-06 23:25:15 +0530

    get loged user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,23 @@
 const express=require('express');
 const router=express.Router();
+const auth=require("../../middleware/auth");
+const Profile=require("../../models/Profile");
+const User=require("../../models/User");
 
+router.get('/me',auth,async(req,res)=>{
+    try{
 
-router.get('/',(req,res)=>{
-    res.send("profile route")
+        const profile=await Profile.findOne({user:req.user.id}).populate("user",["name","avatar"]);
+
+        if(!profile){
+            return res.status(400).json({msg:"There is no profile for this User"});
+        }
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+    // res.send("profile route")
 });
 
 
